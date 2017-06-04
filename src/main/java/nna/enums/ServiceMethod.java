c38b8e58f90bf7af106dd57d32a8e071fe0476d6
PeishuaiLee <1295980850@qq.com>
2017-06-04 21:41:26 +0800

    nna.app.common : to achieve the config not to write code any more

---
+++ src/main/java/nna/enums/ServiceMethod.java
@@ -0,0 +1,16 @@
+package nna.enums;
+
+/**
+ * ${description}
+ *
+ * @author NNA-SHUAI
+ * @create 2017-2017/6/4-21:39
+ */
+public enum ServiceMethod {
+    ManTran,
+    MultiTran,
+    PagingTran,
+    SelectTran,
+    UpdateTran,
+    NonDBTran,
+}
