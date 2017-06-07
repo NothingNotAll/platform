a505bfeca3eefb9d900bbacfb404ef4f01632951
PeishuaiLee <1295980850@qq.com>
2017-06-07 23:58:03 +0800

    this is a meaningful version:we unify the only one MetaBean

--- src/main/java/nna/base/init/NNAServiceInit1.java
+++ src/main/java/nna/base/init/NNAServiceInit1.java
@@ -25,7 +25,6 @@ public class NNAServiceInit1 {
     public static HashMap<Integer,PlatformDB> platformDBMap=new HashMap<Integer, PlatformDB>();
     public static HashMap<Integer,PlatformEntry> platformEntryMap=new HashMap<Integer, PlatformEntry>();
     public static HashMap<Integer,PlatformLog> platformLogMap=new HashMap<Integer, PlatformLog>();
-    public static HashMap<String,PlatformProxy> platformProxyMap=new HashMap<String, PlatformProxy>();
     public static HashMap<Integer,PlatformResource> platformResourceMap=new HashMap<Integer, PlatformResource>();
     public static HashMap<Integer,PlatformRole> platformRoleMap=new HashMap<Integer, PlatformRole>();
     public static HashMap<Integer,PlatformService> platformServiceMap=new HashMap<Integer, PlatformService>();
@@ -37,33 +36,38 @@ public class NNAServiceInit1 {
     public static List serviceTrans=new ArrayList();
     public static List trans=new ArrayList();
     public static List userRoles=new ArrayList();
+    public static List platformProxies=new ArrayList();
 
     public static HashMap<String,PlatformColumn[]> columnMap=new HashMap<String, PlatformColumn[]>();
     public static HashMap<Integer,PlatformRoleResource[]> roleResourceMap=new HashMap<Integer,PlatformRoleResource[]>();
     public static HashMap<String,PlatformServiceTransaction[]> platformServiceTranMap=new HashMap<String, PlatformServiceTransaction[]>();
     public static HashMap<String,PlatformTransaction[]> platformTranMap=new HashMap<String, PlatformTransaction[]>();
     public static HashMap<Integer,PlatformUserRole[]> platformUserRoleMap=new HashMap<Integer, PlatformUserRole[]>();
+    public static HashMap<String,PlatformProxy[]> platformProxyMap=new HashMap<String, PlatformProxy[]>();
 
     public NNAServiceInit1(PreparedStatement[] psts){
         this.psts=psts;
     }
     public void build() throws IllegalAccessException, InvocationTargetException, InstantiationException, SQLException, NoSuchMethodException, ClassNotFoundException {
         appMap=buildIMap(psts[0],"getAppId",Marco.PLATFORM_APP);
-        columnMap=buildIMap(psts[1],"",Marco.PLATFORM_COLUMN);
-        controllerMap=buildSMap(psts[2],"getId",Marco.PLATFORM_CONTROLLER);
-        platformDBMap=buildIMap(psts[3],"getDbId",Marco.PLATFORM_DB);
-        platformEntryMap=buildIMap(psts[4],"",Marco.PLATFORM_ENTRY);
-        platformLogMap=buildIMap(psts[5],"",Marco.PLATFORM_LOG);
-        platformProxyMap=buildIMap(psts[6],"",Marco.PLATFORM_PROXY);
-        platformResourceMap=buildIMap(psts[7],"",Marco.PLATFORM_RESOURCE);
-        platformRoleMap=buildIMap(psts[8],"",Marco.PLATFORM_ROLE);
-        roleResourceMap=buildIMap(psts[9],"",Marco.PLATFORM_ROLE_RESOURCE);
-        platformServiceMap=buildIMap(psts[10],"",Marco.PLATFORM_SERVICE);
-        platformServiceTranMap=buildIMap(psts[11],"",Marco.PLATFORM_SERVICE_TRANSACTION);
-        platformSQLMap=buildIMap(psts[12],"",Marco.PLATFORM_SQL);
-        platformTranMap=buildIMap(psts[13],"",Marco.PLATFORM_TRANSACTION);
-        platformUserMap=buildIMap(psts[14],"",Marco.PLATFORM_USER);
-        platformUserRoleMap=buildIMap(psts[15],"",Marco.PLATFORM_USER_ROLE);
+        controllerMap=buildSMap(psts[1],"getId",Marco.PLATFORM_CONTROLLER);
+        platformDBMap=buildIMap(psts[2],"getDbId",Marco.PLATFORM_DB);
+        platformEntryMap=buildIMap(psts[3],"getEntryId",Marco.PLATFORM_ENTRY);
+        platformLogMap=buildIMap(psts[4],"getLogId",Marco.PLATFORM_LOG);
+
+        platformResourceMap=buildIMap(psts[6],"getResourceId",Marco.PLATFORM_RESOURCE);
+        platformRoleMap=buildIMap(psts[7],"getRoleId",Marco.PLATFORM_ROLE);
+        platformServiceMap=buildSMap(psts[8],"getServiceName",Marco.PLATFORM_SERVICE);
+        platformSQLMap=buildSMap(psts[9],"getSqlId",Marco.PLATFORM_SQL);
+        platformUserMap=buildIMap(psts[10],"getUserId",Marco.PLATFORM_USER);
+
+        getList(columns,psts[11],Marco.PLATFORM_COLUMN);
+        getList(roleResources,psts[12],Marco.PLATFORM_ROLE_RESOURCE);
+        getList(serviceTrans,psts[13],Marco.PLATFORM_SERVICE_TRANSACTION);
+        getList(trans,psts[14],Marco.PLATFORM_TRANSACTION);
+        getList(userRoles,psts[15],Marco.PLATFORM_USER_ROLE);
+        getList(platformProxies,psts[16],Marco.PLATFORM_PROXY);
+
     }
     public HashMap buildIMap(PreparedStatement pst,String methodName,Integer serializableId) throws IllegalAccessException, InstantiationException, SQLException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
         MapTransfer mapTransfer=new MapTransfer();
