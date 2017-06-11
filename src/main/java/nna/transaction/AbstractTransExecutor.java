6345e5da345d754be3cca50408f0f8f1a6d84b6d
PeishuaiLee <1295980850@qq.com>
2017-06-11 21:43:54 +0800

    MetaBean : for Special Trans to use

---
+++ src/main/java/nna/transaction/AbstractTransExecutor.java
@@ -0,0 +1,16 @@
+package nna.transaction;
+
+import java.sql.SQLException;
+
+/**
+ * @author NNA-SHUAI
+ * @create 2017-06-11 21:35
+ **/
+
+public class AbstractTransExecutor implements TransExecutor{
+
+    public Object executeTransactions(MetaBeanWrapper metaBeanWrapper) throws SQLException {
+        
+        return null;
+    }
+}
