a4175faa831b217129a1a51b1bdef65348c44bdf
PeishuaiLee <1295980850@qq.com>
2017-06-11 23:16:27 +0800

    MetaBean : add NIO Server

---
+++ src/main/java/nna/base/server/NIOServer.java
@@ -0,0 +1,81 @@
+package nna.base.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketOption;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author NNA-SHUAI
+ * @create 2017-06-11 22:04
+ **/
+
+public class NIOServer {
+    private Server[] servers;
+    private ServerSocketChannel[] serverSocketChannels;
+    private Selector selector= SelectorProvider.provider().openSelector();
+
+    public NIOServer(Server[] servers) throws IOException {
+        int count=servers.length;
+        serverSocketChannels=new ServerSocketChannel[count];
+    }
+
+    public void buildServers() throws IOException {
+        int serverCount=servers.length;
+        Server server;
+        for(int index=0;index < serverCount;index++){
+            server=servers[index];
+            ServerSocketChannel serverSocketChannel=ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            InetSocketAddress inetSocketAddress=new InetSocketAddress(server.getIp(),server.getPort());
+            serverSocketChannel.bind(inetSocketAddress,server.getBackLog());
+            SocketOption[] options=server.getSocketOptions();
+            int count=options.length;
+            SocketOption temp;
+            for(index=0;index < count;index++){
+                temp=options[index];
+                serverSocketChannel.setOption(temp,temp);
+            }
+            serverSocketChannels[index]=serverSocketChannel;
+            serverSocketChannel.register(selector,SelectionKey.OP_CONNECT,server.getAttach());
+        }
+    }
+
+    public void listen(){
+        try{
+            while(true){
+                selector.select();
+                Set<SelectionKey> set=selector.keys();
+                Iterator<SelectionKey> iterator=set.iterator();
+                SelectionKey temp;
+                while(iterator.hasNext()){
+                    temp=iterator.next();
+                    int selectionKey=temp.interestOps();
+                    switch (selectionKey){
+                        case SelectionKey.OP_ACCEPT:
+                            ;
+                            break;
+                        case SelectionKey.OP_READ:
+                            ;
+                            break;
+                        case SelectionKey.OP_WRITE:
+                            ;
+                            break;
+                        case SelectionKey.OP_CONNECT:
+                            ;
+                            break;
+                    }
+                }
+            }
+        }catch (Exception e){
+
+        }finally {
+
+        }
+    }
+}
--- src/main/java/nna/base/server/Server.java
+++ src/main/java/nna/base/server/Server.java
@@ -1,21 +1,49 @@
 package nna.base.server;
 
+import java.lang.management.ManagementFactory;
 import java.lang.reflect.Method;
+import java.net.SocketOption;
 
 /**
  * @author NNA-SHUAI
  * @create 2017-06-11 13:08
  **/
 
-public class Server {
+public class Server<T> {
+    private static String pid;
+    private static String jvmName;
+
+
     public static void main(String[] args){
         System.out.println(Runtime.getRuntime().availableProcessors());
     }
     private String ip="127.0.0.1";
     private int port;
-    private int pid;
+    private int backLog;
+    private SocketOption<Object>[] socketOptions;
+    private T attach=(T)this;
+    private Object selectionKeyAttach;
     private Method serverMethod;
     private Object serverObject;
+
+    static {
+        setJVMCfg();
+    }
+
+    private static void setJVMCfg(){
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        Server.setJvmName(name);
+        String pid = name.split("@")[0];
+        Server.setPid(pid);
+    }
+
+    public static String getJvmName() {
+        return jvmName;
+    }
+
+    public static void setJvmName(String jvmName) {
+        Server.jvmName = jvmName;
+    }
 
     public String getIp() {
         return ip;
@@ -33,12 +61,12 @@ public class Server {
         this.port = port;
     }
 
-    public int getPid() {
-        return pid;
+    public static String getPid() {
+        return Server.pid;
     }
 
-    public void setPid(int pid) {
-        this.pid = pid;
+    public static void setPid(String pid) {
+        Server.pid = pid;
     }
 
     public Method getServerMethod() {
@@ -55,5 +83,37 @@ public class Server {
 
     public void setServerObject(Object serverObject) {
         this.serverObject = serverObject;
+    }
+
+    public int getBackLog() {
+        return backLog;
+    }
+
+    public void setBackLog(int backLog) {
+        this.backLog = backLog;
+    }
+
+    public SocketOption<Object>[] getSocketOptions() {
+        return socketOptions;
+    }
+
+    public void setSocketOptions(SocketOption<Object>[] socketOptions) {
+        this.socketOptions = socketOptions;
+    }
+
+    public T getAttach() {
+        return attach;
+    }
+
+    public void setAttach(T attach) {
+        this.attach = attach;
+    }
+
+    public Object getSelectionKeyAttach() {
+        return selectionKeyAttach;
+    }
+
+    public void setSelectionKeyAttach(Object selectionKeyAttach) {
+        this.selectionKeyAttach = selectionKeyAttach;
     }
 }
