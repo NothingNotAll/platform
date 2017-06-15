0d34a69d08f76d7d0d31f709d276a71aecc0cb58
PeishuaiLee <1295980850@qq.com>
2017-06-16 00:53:04 +0800

    Concurrent FrameWork Next Step is solve the oom problem

---
+++ src/main/java/nna/base/util/concurrent/Monitor.java
@@ -0,0 +1,47 @@
+package nna.base.util.concurrent;
+
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @author NNA-SHUAI
+ * @create 2017-06-15 21:53
+ **/
+
+public class Monitor implements Runnable {
+    SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyyMMdd-HH:mm:ss:SSS");
+    private Worker[] workers;
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    public void run() {
+        while(true){
+            for(Worker worker:workers){
+                System.out.println(simpleDateFormat.format(System.currentTimeMillis())+"-"+worker.getLoadNo()+"-"+worker.getWorkMap().size());
+            }
+            try {
+                Thread.currentThread().sleep(3000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("--");
+        }
+    }
+
+    public Worker[] getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(Worker[] workers) {
+        this.workers = workers;
+    }
+}
