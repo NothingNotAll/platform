89e38be7d4dab1d61f75b5607ea1d366210d4f5e
PeishuaiLee <1295980850@qq.com>
2017-06-17 10:34:48 +0800

    protocolType

--- src/main/java/nna/base/bean/dbbean/PlatformProtocol.java
+++ src/main/java/nna/base/bean/dbbean/PlatformProtocol.java
@@ -1,15 +1,18 @@
 package nna.base.bean.dbbean;
 
+import nna.base.bean.Clone;
+import nna.enums.ProtocolType;
+
 /**
  * @author NNA-SHUAI
  * @create 2017-06-11 13:01
  **/
 
-public class PlatformProtocol {
+public class PlatformProtocol extends Clone{
+    private static final Long serialVersionUID=23L;
     private int entryId;
-    private String entryClass;
-    private String entryMethod;
     private int entryPort;
+    private ProtocolType protocolType;
 
     public int getEntryId() {
         return entryId;
@@ -19,27 +22,19 @@ public class PlatformProtocol {
         this.entryId = entryId;
     }
 
-    public String getEntryClass() {
-        return entryClass;
-    }
-
-    public void setEntryClass(String entryClass) {
-        this.entryClass = entryClass;
-    }
-
-    public String getEntryMethod() {
-        return entryMethod;
-    }
-
-    public void setEntryMethod(String entryMethod) {
-        this.entryMethod = entryMethod;
-    }
-
     public int getEntryPort() {
         return entryPort;
     }
 
     public void setEntryPort(int entryPort) {
         this.entryPort = entryPort;
+    }
+
+    public ProtocolType getProtocolType() {
+        return protocolType;
+    }
+
+    public void setProtocolType(ProtocolType protocolType) {
+        this.protocolType = protocolType;
     }
 }
