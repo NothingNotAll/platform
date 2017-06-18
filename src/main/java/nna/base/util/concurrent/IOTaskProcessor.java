6de508186c26c79e63f1dbeef85615dddcea9ee7
PeishuaiLee <1295980850@qq.com>
2017-06-18 13:22:11 +0800

    Concurrent FrameWork Add Real IO Processor

---
+++ src/main/java/nna/base/util/concurrent/IOTaskProcessor.java
@@ -0,0 +1,14 @@
+package nna.base.util.concurrent;
+
+/**
+ * the real io worker
+ * @author NNA-SHUAI
+ * @create 2017-06-18 13:21
+ **/
+
+public class IOTaskProcessor implements Runnable {
+
+    public void run() {
+
+    }
+}
