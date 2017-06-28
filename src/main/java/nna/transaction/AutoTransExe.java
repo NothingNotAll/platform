0c5551c6c92686de8372665cf55e839f9e09ad0d
PeishuaiLee <1295980850@qq.com>
2017-06-28 16:40:06 +0800

    auto trans exe to avoid write if else codes

---
+++ src/main/java/nna/transaction/AutoTransExe.java
@@ -0,0 +1,18 @@
+package nna.transaction;
+
+import java.sql.SQLException;
+
+/**
+ * auto tran to exe avoid write if else business codes
+ *
+ * @author NNA-SHUAI
+ * @create 2017-06-28 16:39
+ **/
+
+public class AutoTransExe implements TransExecutor {
+
+    public Object executeTransactions(MetaBeanWrapper metaBeanWrapper) throws SQLException {
+
+        return null;
+    }
+}
