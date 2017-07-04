2840da383a9a3c60bea852c2d210427523b658d5
PeishuaiLee <1295980850@qq.com>
2017-07-04 22:14:55 +0800

    add nio server test example

--- src/main/java/nna/test/TradeTest.java
+++ src/main/java/nna/test/TradeTest.java
@@ -8,7 +8,9 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 
 /**
@@ -31,37 +33,12 @@ public class TradeTest {
         SocketChannel client=SocketChannel.open();
         client.connect(new InetSocketAddress(IP,PORT));
         byte[] bytes=xmlStr.getBytes(XML_ENCODE);
+        System.out.println(xmlStr);
         ByteBuffer byteBuffer=ByteBuffer.wrap(bytes);
         client.write(byteBuffer);
         client.shutdownOutput();
-        byte[] length=new byte[HEAD_LENTH];
-        ByteBuffer rspStrLength=ByteBuffer.wrap(length);
-        int readCount=0;
-        while(readCount<8){
-            readCount+=client.read(rspStrLength);
-        }
-        Integer rspSize=new Integer(new String(length));
-        byte[] body=new byte[rspSize];
-        ByteBuffer rspBB=ByteBuffer.wrap(body);
-        readCount=0;
-        while(readCount<=rspSize){
-            int readCounts=client.read(rspBB);
-            if(readCounts!=-1){
-                readCount+=readCounts;
-            }else{
-                break;
-            }
-        }
         client.shutdownInput();
         client.close();
-        String rspStr=new String(body,XML_DECODE);
         HashMap<String,String[]> rspMap=new HashMap<String, String[]>();
-        try {
-            XmlUtil.parseXmlStr(rspStr,rspMap);
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        }
-        System.out.println(rspStr);
-        System.out.println(rspMap.toString());
     }
 }
