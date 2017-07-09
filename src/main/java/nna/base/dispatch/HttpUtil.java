9c3e13f4d06bff18898a79acf2728732da47af78
PeishuaiLee <1295980850@qq.com>
2017-07-09 22:14:43 +0800

    simplify http parsing

--- src/main/java/nna/base/dispatch/HttpUtil.java
+++ src/main/java/nna/base/dispatch/HttpUtil.java
@@ -1,5 +1,8 @@
 package nna.base.dispatch;
 
+import java.io.BufferedReader;
+import java.io.CharArrayReader;
+import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.HashMap;
 
@@ -7,8 +10,47 @@ import java.util.HashMap;
  * Created by NNA-SHUAI on 2017/7/9.
  */
 public class HttpUtil {
-    private HttpUtil(){}
 
+    private HttpUtil(){}
+    static void parseHttpRequest(HashMap<String,String[]> headers,HashMap<String,String[]> kvMap,String request) throws IOException {
+            BufferedReader lineReader=new BufferedReader(new CharArrayReader(request.toCharArray()));
+            String line;
+            while(true){
+                line=lineReader.readLine();
+                if(line!=null){
+                    break;
+                }
+            }
+            boolean isGETMethod=parseFirstLine(headers,kvMap,line);
+            while(true){
+                line=lineReader.readLine();
+                if(line.trim().equals("")){
+                    break;
+                }
+                parseHeaders(headers,line);
+            }
+            if(isGETMethod){
+                return ;
+            }
+            String boundary=null;
+            String contentType=headers.get("Content-Type")[0];
+            if(contentType!=null&&contentType.toLowerCase().startsWith("multipart")){
+                String multi=headers.get("Content-Type")[0];
+                boundary=multi.split("[;]")[1].split("[=]")[1].trim();
+            }
+            while(true){
+                line=lineReader.readLine();
+                if(line!=null){
+                    break;
+                }
+            }
+            if(boundary!=null){
+                if(line.trim().equals(boundary)){
+                    return;
+                }
+            }
+            parseKeyValues(kvMap,line);
+    }
     static boolean parseFirstLine(HashMap<String,String[]> headers,HashMap<String,String[]> kvMap,String firstLine){
         boolean isMethodGET=false;
         String[] methodAndURIAndKVAndHttpVersion=firstLine.split("[\\s]");
@@ -28,22 +70,39 @@ public class HttpUtil {
         return isMethodGET;
     }
 
-    static boolean parseHeaders(HashMap<String,String[]> headers,String headerLine){
-        boolean isUpload=false;
-        String[] headerKV=headerLine.split("[:]");
-        headers.put(headerKV[0].trim(),new String[]{headerKV[1].trim()});
-        if(headers.get("Content-Type")[0].toLowerCase().startsWith("multipart")){
-            isUpload=true;
+    static void parseHeaders(HashMap<String,String[]> headers,String headerLine){
+        if(headerLine!=null&&!headerLine.trim().equals("")){
+            String[] headerKV=headerLine.split("[:]");
+            headers.put(headerKV[0].trim(),new String[]{headerKV[1].trim()});
+            System.out.println(headerKV[0]+":"+headerKV[1]);
         }
-        return isUpload;
     }
 
     static void parseKeyValues(HashMap<String,String[]> kvMap,String kvLine){
         String[] kvs=kvLine.split("[&]");
-        String[] kv;
-        for(String kvStr:kvs){
-            kv=kvStr.split("[:]");
-            kvMap.put(kv[0].trim(), new String[]{URLDecoder.decode(kv[1])});
+        if(kvs!=null&&kvs.length>=1){
+            String[] kv;
+            for(String kvStr:kvs){
+                kv=kvStr.split("[=]");
+                kvMap.put(kv[0].trim(), new String[]{URLDecoder.decode(kv[1])});
+                System.out.println(kv[0]+":"+kv[1]);
+            }
         }
+    }
+
+    static String getHttpResponseHeader(){
+        return new String("HTTP/1.1 200 OK\r\n" +
+                "Date: Sat, 31 Dec 2005 23:59:59 GMT\r\n" +
+                "Content-Type: text/html;charset=UTF-8\r\n" +
+                "Content-Length: 122\r\n" +
+                "\r\n" +
+                "＜html＞\n" +
+                "＜head＞\n" +
+                "＜title＞Wrox Homepage＜/title＞\n" +
+                "＜/head＞\n" +
+                "＜body＞\n" +
+                "＜!-- body goes here --＞\n" +
+                "＜/body＞\n" +
+                "＜/html＞");
     }
 }
--- src/main/java/nna/base/dispatch/Protocol.java
+++ src/main/java/nna/base/dispatch/Protocol.java
@@ -91,149 +91,27 @@ public class Protocol {
         }
     }
 
-    private static byte[] read(SocketChannel channel) throws IOException {
-        LinkedList<byte[]> bytes=new LinkedList<byte[]>();
-        byte[] temp;
-        ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
-        int totalCount=0;
-        int readCount;
-        while(true){
-                byteBuffer.clear();
-                readCount=channel.read(byteBuffer);
-                if(readCount==-1){
-                    break;
-                }
-                if(readCount>0){
-                    totalCount+=readCount;
-                    byteBuffer.flip();
-                    temp=new byte[readCount];
-                    byteBuffer.get(temp);
-                    bytes.add(temp);
-                }
-        }
-        int index2;
-        int index3=0;
-        byte[] byteList=new byte[totalCount];
-        int size=bytes.size();
-        int length;
-        for(int index=0;index < size;index++){
-            temp=bytes.get(index);
-            length=temp.length;
-            index2=0;
-            for(;index2<length;index2++){
-                byteList[index3++]=temp[index2];
-            }
-        }
-        return byteList;
-    }
-
-    public static String processHttp(SocketChannel socketChannel) throws IOException {
-        byte[] bytes=read(socketChannel);
-        Long end=System.currentTimeMillis();
+    public static String processHttp(SocketChannel socketChannel) {
         try {
-            System.out.println("read time end:"+simpleDateFormat.format(end)+" from client:"+new String(bytes,0,bytes.length,"UTF-8"));
-        } catch (UnsupportedEncodingException e) {
+            byte[] bytes=BytesUtil.readBytes(socketChannel);
+            HashMap<String,String[]> headers=new HashMap<String, String[]>();
+            HashMap<String,String[]> kvs=new HashMap<String, String[]>();
+            HttpUtil.parseHttpRequest(headers,kvs,new String(bytes,"UTF-8"));
+        } catch (IOException e) {
             e.printStackTrace();
+            try {
+                socketChannel.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
         }
-        HashMap<String,String[]> map=new HashMap<String, String[]>();
-        toMap(bytes,map);
-        socketChannel.write(ByteBuffer.wrap(getHttpResponseHeader().getBytes("UTF-8")));
-        socketChannel.close();
         return null;
-    }
-
-    private static void toMap(byte[] bytes,HashMap<String,String[]> map) throws IOException {
-        BufferedReader lineReader=new BufferedReader(new CharArrayReader(new String(bytes,"UTF-8").toCharArray()));
-        String line;
-        String[] strs;
-        String method;
-        String link;
-        String[] kvs;
-        while(true){
-            line=lineReader.readLine();
-            if(line!=null){
-                strs=line.split("[\\s]");
-                method=strs[0];
-                link=strs[1];
-                map.put("HTTP_METHOD",new String[]{method});
-                map.put("HTTP_VERSION",new String[]{strs[2]});
-                if(method.equals("GET")){
-                    strs=strs[1].split("[?]");
-                    link=strs[0];
-                    if(strs.length>1){
-                        strs=strs[1].split("[&]");
-                        for(String kv:strs){
-                            kvs=kv.split("[=]");
-                            map.put(kvs[0].trim(),new String[]{URLDecoder.decode(kvs[1].trim())});
-                            System.out.println(kvs[0]+":"+URLDecoder.decode(kvs[1]));
-                        }
-                    }
-                }
-                map.put("HTTP_METHOD",new String[]{link});
-                break;
-            }else{
-                continue;
-            }
-        }
-        while(true){
-            line=lineReader.readLine();
-            if(line!=null){
-                if(line.trim().equals("")){
-                    break;
-                }
-                kvs=line.split("[:]");
-                map.put(kvs[0].trim(),new String[]{kvs[1].trim()});
-                System.out.println(kvs[0]+":"+URLDecoder.decode(kvs[1]));
-                continue;
-            }else{
-                break;
-            }
-        }
-
-        if(method.equals("GET")){
-            return ;
-        }
-
-        if(map.get("Content-Type")[0].toLowerCase().startsWith("multipart")){
-            return ;
-        }
-
-        while(true){
-            line=lineReader.readLine();
-            if(line!=null){
-                strs=line.split("[&]");
-                for(String kv:strs){
-                    kvs=kv.split("[=]");
-                    map.put(kvs[0].trim(),new String[]{URLDecoder.decode(kvs[1].trim())});
-                    System.out.println(kvs[0]+":"+URLDecoder.decode(kvs[1]));
-                }
-            }else{
-                return;
-            }
-        }
-
-    }
-
-    private static String getHttpResponseHeader(){
-        return new String("HTTP/1.1 200 OK\r\n" +
-                "Date: Sat, 31 Dec 2005 23:59:59 GMT\r\n" +
-                "Content-Type: text/html;charset=UTF-8\r\n" +
-                "Content-Length: 122\r\n" +
-                "\r\n" +
-                "＜html＞\n" +
-                "＜head＞\n" +
-                "＜title＞Wrox Homepage＜/title＞\n" +
-                "＜/head＞\n" +
-                "＜body＞\n" +
-                "＜!-- body goes here --＞\n" +
-                "＜/body＞\n" +
-                "＜/html＞");
     }
 
     public static String processXml(SocketChannel channel) {
         byte[] bytes;
         try {
-            bytes=read(channel);
+            bytes=BytesUtil.readBytes(channel);
             HashMap<String,String[]> map=new HashMap<String, String[]>();
             XmlUtil.parseXmlStr(new String(bytes),map);
             String responseStr=service(map);
